74338005fae7a0f75ce27146abb68ad833234b05
arun <nuranisafirdausy1201@gmail.com>
2022-11-07 10:22:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ echo "Hello world! <br>";
+
+ $nama = "Arun";
+ $umur = 21;
+
+ echo "Nama saya $nama, saya berusia $umur tahun. <br>";
+
+ $namaAdik= "Aan";
+ $umurAdik= 16;
+
+ echo "Nama adik saya $namaAdik, saya berusia $umurAdik tahun. <br>";
+
+ $selisihUmur = $umur - $umurAdik;
+
+ echo "Selisih umur saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
